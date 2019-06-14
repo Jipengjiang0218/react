f05772ae996e4a193eb5efe77a45c4596b53bef6
tiany <jipengjiag>
2019-06-14 18:32:30 +0800

    'react'

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import {HashRouter,Route,Switch} from 'react-router-dom'
+import Login from './pages/login/login.jsx'
+import Admin from './pages/admin/admin.jsx'
+
+export default class App extends Component{
+    render(){
+      return (      
+        <HashRouter>
+          <Switch>
+            <Route path="/login" component={Login}/>
+            <Route path="/" component={Admin}/>
+          </Switch>  
+        </HashRouter>  
+      )
+    }
+}
